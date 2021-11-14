64223025c5152c8cd8d502321a5a920c52bdb1b3
Gareth-Yao <57732248+Gareth-Yao@users.noreply.github.com>
2021-11-13 18:04:13 -0600

    Added Basic Structure. Added satellite input fomr

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import '../styles/App.css';
 import Header from "./Header"
 import Footer from "./Footer"
+import Main from "./Main"
 
 function App() {
   return (
     <div className="App">
       <Header/>
+        <Main/>
        <Footer/>
     </div>
   );
